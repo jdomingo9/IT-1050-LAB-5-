6ba66144eaa59c6851e52337d214bf2e3a1ee8b4
jdomingo9 <domingojohn51@gmail.com>
2016-05-10 12:53:24 -0400

    lolol

---
+++ IT 1050 Lab 5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_1050_Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
